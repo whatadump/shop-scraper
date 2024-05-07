9d35651d12d2479c6fc8d870e086f27f73182c39
Takhir Latypov <cregennandev@gmail.com>
2024-05-07 22:29:38 +0300

    Добавлен мигратор

--- ShopScraper.Domain/Bundle.cs
+++ ShopScraper.Domain/Bundle.cs
@@ -1,12 +1,15 @@
 ﻿namespace ShopScraper.Domain
 {
+    using HostedServices;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
 
     public static class Bundle
     {
         public static IServiceCollection UseDomainServices(this IServiceCollection services, IConfigurationRoot configuration)
-        {   
+        {
+            services.AddHostedService<MigratorHostedService>();
+            
             
             return services;
         } 
---
+++ ShopScraper.Domain/HostedServices/MigratorHostedService.cs
@@ -0,0 +1,24 @@
+﻿namespace ShopScraper.Domain.HostedServices;
+
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+public class MigratorHostedService(IServiceProvider provider, ILogger<MigratorHostedService> logger) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        using var scope = provider.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        logger.LogInformation("Накатываем миграции");
+        await context.Database.MigrateAsync(cancellationToken: cancellationToken);
+        logger.LogInformation("Накатка миграций завершена");
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
